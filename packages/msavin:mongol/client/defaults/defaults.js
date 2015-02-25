49e91b583346877a9e4f442c2dc72482ff2e250e
Main <main@Coffees-MacBook-Pro.local>
2015-02-25 17:48:19 -0500

    - Added notice to encourage users to switch to automatic collection detection, and report if there are any issues - Code cleanup

--- packages/msavin:mongol/client/defaults/defaults.js
+++ packages/msavin:mongol/client/defaults/defaults.js
@@ -15,12 +15,19 @@ Meteor.startup(function() {
 		
 		var defaults = {
 			'collections':    collections, 
-			'display':        false,
 		}
 	
 		Session.set("Mongol", defaults);
 	  
-	} 
+	}  else {
+		console.log("");
+		console.log('Mongol now detects collections automatically');
+		console.log('Please remove the Session configuration you had set earlier');
+		console.log('If you have any issues, please report them to:');
+		console.log('https://github.com/msavin/Mongol');
+		console.log("");
+
+	}
   
 });
 
