42d67235a26bf349be0f94a0f973c1f8c3fd0fa0
Alexander Weber <alexander.weber@pro-crm.de>
2015-02-26 18:11:38 +0100

    FIXME: undefined vars? minor code cleanup

--- packages/msavin:mongol/client/docViewer/docViewer.js
+++ packages/msavin:mongol/client/docViewer/docViewer.js
@@ -1,60 +1,63 @@
 Template.Mongol_docViewer.helpers({
-	activeDocument: function () {
-		var collectionName    = String(this);
-		var currentCollection = Mongol.Collection(collectionName);
-		var documents         = currentCollection.find().fetch();
-		var sessionKey  = "Mongol_" + String(this);
-		var docNumber   = Session.get(sessionKey);
-		var docCurrent  = documents[docNumber];
-		return docCurrent;
-	},
-	documentJSON: function () {
-		var docCurrent = this;
-		var json_output = JSON.stringify(docCurrent, null, 2);
-			
-			if (! (typeof json_output === "undefined")) {
-				colorize    = MongolPackage.colorize(json_output);
-			} else {
-				colorize    = json_output;
-			}
+  activeDocument: function () {
+    var collectionName = String(this);
+    var currentCollection = Mongol.Collection(collectionName);
+    var documents = currentCollection.find().fetch();
+    var sessionKey = "Mongol_" + String(this);
+    var docNumber = Session.get(sessionKey);
+    var docCurrent = documents[docNumber];
+    return docCurrent;
+  },
+  documentJSON: function () {
+    var docCurrent = this;
+    var json_output = JSON.stringify(docCurrent, null, 2);
 
-			return colorize;
+    // FIXME: variable colorize is not defined - or is it some global var from somewhere?
 
-	},
-	editContent: function () {
-		
-		var editMode = Session.get("Mongol_editMode");
-		
-		if (editMode) {
-			return "true";
-		}
+    if (!(json_output === undefined)) {
+      colorize = MongolPackage.colorize(json_output);
+    } else {
+      colorize = json_output;
+    }
 
-	},
-	editStyle: function () {
-		
-		var editMode = Session.get("Mongol_editMode");
-		
-		if (editMode) {
-			return "Mongol_editable";
-		}
-		
-	},
-	notEmpty: function() {
-		documentCount = Mongol.Collection(String(this)) && Mongol.Collection(String(this)).find().count() || 0;
-		if (documentCount >= 1) {
-			return true;
-		}
-	}
+    return colorize;
+
+  },
+  editContent: function () {
+
+    var editMode = Session.get("Mongol_editMode");
+
+    if (editMode) {
+      return "true";
+    }
+
+  },
+  editStyle: function () {
+
+    var editMode = Session.get("Mongol_editMode");
+
+    if (editMode) {
+      return "Mongol_editable";
+    }
+
+  },
+  notEmpty: function () {
+    // FIXME: variable documentCount is not defined - or is it some global var from somewhere?
+    documentCount = Mongol.Collection(String(this)) && Mongol.Collection(String(this)).find().count() || 0;
+    if (documentCount >= 1) {
+      return true;
+    }
+  }
 });
 
 // Will possibly be used in augmented document udpate UI
 /*Template.Mongol_docViewer.events({
-	
+
 	'click .Mongol_string' : function (evt,tmpl) {
 		var field = $(evt.target).prevAll(".Mongol_key:first").text().slice(1,-2);
 		Session.set('Mongol_inlineEdit',true);
 		Tracker.flush();
 		// Do something to trigger the editable text element
 	}
-	
+
 });*/
