27c20edaca0e8f6786365ff8c9dace81ea958107
Alexander Weber <alexander.weber@pro-crm.de>
2015-02-26 18:03:00 +0100

    FIXME: unnessary code minor code cleanup

--- packages/msavin:mongol/client/collections/collections.js
+++ packages/msavin:mongol/client/collections/collections.js
@@ -1,65 +1,66 @@
 Template.Mongol_collection.events({
-	'click .Mongol_toggle_selected_collection': function (evt) {
+  'click .Mongol_toggle_selected_collection': function (evt) {
 
-		var targetCollection =  String(this);
-		var sessionKey       = "Mongol_" + targetCollection;
-		
-		if (Session.equals("Mongol_currentCollection",targetCollection)) {
-			
-			// either do nothing or collapse the pane
-			// comment out the line below for not collapsing the pane
-			Session.set("Mongol_currentCollection",null);
-			
-		} else {
-			
-			Session.set("Mongol_editMode", false)
+    var targetCollection = String(this);
+    var sessionKey = "Mongol_" + targetCollection;
 
-			var thisCollection = Mongol.Collection(targetCollection);
-			var documentCount = thisCollection.find().count();
+    if (Session.equals("Mongol_currentCollection", targetCollection)) {
 
-			// If the collection doesn't have an index key set,
-			// start it from the first document
-			if (!Session.get(sessionKey)) {
-				Session.set(sessionKey, 0);
-			}
+      // either do nothing or collapse the pane
+      // comment out the line below for not collapsing the pane
+      Session.set("Mongol_currentCollection", null);
 
-			Session.set("Mongol_currentCollection", targetCollection);
-			
-		}
-		
-	}
+    } else {
+
+      Session.set("Mongol_editMode", false);
+
+      // FIXME: seems not to be used
+      //var thisCollection = Mongol.Collection(targetCollection);
+      //var documentCount = thisCollection.find().count();
+
+      // If the collection doesn't have an index key set,
+      // start it from the first document
+      if (!Session.get(sessionKey)) {
+        Session.set(sessionKey, 0);
+      }
+
+      Session.set("Mongol_currentCollection", targetCollection);
+
+    }
+
+  }
 });
 
 Template.Mongol_collection.helpers({
-	active: function () {
-		
-		var currentCollection = Session.get("Mongol_currentCollection"),
-			targetCollection  = String(this);
+  active: function () {
 
-		if (currentCollection === targetCollection) {
-			return "Mongol_row_expand";
-		} 
-		
-	},
-	collectionCount: function () {
+    var currentCollection = Session.get("Mongol_currentCollection"),
+      targetCollection = String(this);
 
-		var collectionName = String(this);
-		var collectionVar  = Mongol.Collection(collectionName);
-			
-		var count          = collectionVar && collectionVar.find().count() || 0;
+    if (currentCollection === targetCollection) {
+      return "Mongol_row_expand";
+    }
 
-		return count;
+  },
+  collectionCount: function () {
 
-	},
-	currentPosition: function () {
-		
-		var targetCollection = String(this);
-		var sessionKey       = "Mongol_" + targetCollection;
-		
-		var current = Session.get(sessionKey);
-		var count = current + 1;
-		
-		return count;
-		
-	}
+    var collectionName = String(this);
+    var collectionVar = Mongol.Collection(collectionName);
+
+    var count = collectionVar && collectionVar.find().count() || 0;
+
+    return count;
+
+  },
+  currentPosition: function () {
+
+    var targetCollection = String(this);
+    var sessionKey = "Mongol_" + targetCollection;
+
+    var current = Session.get(sessionKey);
+    var count = current + 1;
+
+    return count;
+
+  }
 });
