613fc04b9f05c3ea02a2de2456de3be7e7722cb6
Brent Abrahams <brent_abrahams@yahoo.com>
2015-03-02 09:28:49 +0800

    Added hotkey support (ctrl + F) for fullscreen

--- packages/msavin:mongol/client/header/header.js
+++ packages/msavin:mongol/client/header/header.js
@@ -12,11 +12,7 @@ Template.Mongol_header.events({
   },
   'click .Mongol_FullScreen' : function (e) {
     e.stopPropagation();
-    Session.set("Mongol_fullscreen", !Session.get("Mongol_fullscreen"));
-    if (Session.equals("Mongol_currentCollection", null)) {
-	  Session.set("Mongol_fullscreen", true);
-      Session.set("Mongol_currentCollection", "mongol_618");
-    }
+	MongolPackage.toggleFullScreen();
   }
 });
 
