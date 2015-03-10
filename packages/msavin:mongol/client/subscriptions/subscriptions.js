c82972b994fa7e4f9a46730044fd32d2d356b9cc
Main <main@Coffees-MacBook-Pro.local>
2015-03-10 13:13:34 -0700

    Updated appearance of Mongol, laid groundwork for subscriptions

--- packages/msavin:mongol/client/subscriptions/subscriptions.js
+++ packages/msavin:mongol/client/subscriptions/subscriptions.js
@@ -1,3 +1,23 @@
+Template.Mongol_subscriptions.helpers({
+  active: function () {
+    if (Session.equals("Mongol_currentCollection", "subscriptions_618")) {
+      return "Mongol_row_expand";
+    }
+  }
+});
+
+
+Template.Mongol_subscriptions.events({
+  'click .Mongol_toggle_selected_collection': function () {
+    if (Session.equals("Mongol_currentCollection", "subscriptions_618")) {
+      Session.set("Mongol_currentCollection", null);
+    } else {
+      Session.set("Mongol_currentCollection", "subscriptions_618");
+    }
+  }
+});
+
+
 // Ssh.. this is supposed to be a surprise :)
 
 // Object for subscriptions
@@ -5,39 +25,39 @@
 // Object.observe polyfill
 
 // Template.Mongol_subscriptions.helpers({
-// 	subscription: function () {
-// 		var data = MongolSubData.get()
-// 		return data;
-// 	},
-// 	subscriptionParams: function() {
-// 		return this.params
-// 	}
+//  subscription: function () {
+//    var data = MongolSubData.get()
+//    return data;
+//  },
+//  subscriptionParams: function() {
+//    return this.params
+//  }
 // });
 
 // Template.Mongol_subscriptions.events({
-// 	'click .Mongol_stop_subscription': function () {
-// 		this.stop()
-// 	},
-// 	'click #createNewSub': function () {
+//  'click .Mongol_stop_subscription': function () {
+//    this.stop()
+//  },
+//  'click #createNewSub': function () {
 
-// 		var argument = false,
-// 			stuff = [];
-		
-// 		var askForArgument = function () {
-// 			argument = prompt("What is the name of your subscription?");
-// 			addArgument(argument);
-// 		}
-		
-// 		var addArgument = function (argument) {
-// 			if (argument) {
-// 				stuff.push(argument);
-// 				askForArgument();
-// 			} else {
-// 				Meteor.subscribe.apply(Meteor, stuff); 
-// 			}
-// 		}
+//    var argument = false,
+//      stuff = [];
+    
+//    var askForArgument = function () {
+//      argument = prompt("What is the name of your subscription?");
+//      addArgument(argument);
+//    }
+    
+//    var addArgument = function (argument) {
+//      if (argument) {
+//        stuff.push(argument);
+//        askForArgument();
+//      } else {
+//        Meteor.subscribe.apply(Meteor, stuff); 
+//      }
+//    }
 
-// 		askForArgument();
+//    askForArgument();
 
-// 	}
+//  }
 // });
