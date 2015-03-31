c3fa3b24976b891c75190f61f0a653b356b8ec52
Brent Abrahams <brent_abrahams@yahoo.com>
2015-03-31 17:13:19 +0800

    Updated dependency on editable-json package

--- package.js
+++ package.js
@@ -47,7 +47,7 @@ Package.onUse(function(api) {
   api.use('aldeed:collection2@2.3.2', {weak: true}); // This must go before: api.use('dburles:mongo-collection-instances@0.3.1');
   api.use('dburles:mongo-collection-instances@0.3.3', 'client');
   api.use('gwendall:session-json@0.1.7', 'client');
-  api.use('babrahams:editable-json@0.4.0');
+  api.use('babrahams:editable-json@0.4.1');
   api.use('fortawesome:fontawesome@4.3.0', 'client');
   api.use('meteortoys:toykit@0.2.2');
 
